8f26168904343d9b198cd45ee8a327b1677292f3
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-05-29 01:35:22 -0500

    avanzando en el uso de la API de google maps ubicación

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ botonPublicar[0].addEventListener("click", function () {
   div.appendChild(h5);
   div.appendChild(parrafo);
   areaPublicacion.appendChild(div);
-  div.className = "container " + "center " +"card-panel " + "hoverable";
+  div.className = "container " + "center " + "card-panel " + "hoverable";
   areaTitulo.value = " ";
   areaMensaje.value = " ";
   fondoModal[0].style.display = "none";
@@ -97,21 +97,63 @@ botonPublicar[1].addEventListener("click", function (e) {
 
 
 /* Evento y geolocalización*/
-var mapa = document.getElementById("map");
 
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 6
+    });
+    var infoWindow = new google.maps.InfoWindow({
+      map: map
+    });
 
+    
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+  }
+
+var areaTituloEvento = document.getElementById("modal-event-title");
+var mapa = document.getElementById("map");
 botonPublicar[2].addEventListener("click", function () {
+  // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+      }, function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+  var tituloEvento = areaTituloEvento.value;
+  var divContenedor = document.createElement("div");
+  var h5 = document.createElement("h5");
+  h5.textContent = tituloEvento;
+  divContenedor.className = "container " + "center " + "card-panel " + "hoverable";
+  mapa.className = "center";
+  divContenedor.appendChild(h5);
+  divContenedor.appendChild(mapa);
+  mapa.style.display = "block";
+  areaPublicacion.appendChild(divContenedor);
+
   fondoModal[2].style.display = "none";
   modal[2].style.display = "none";
   mapa.style.display = "flex";
